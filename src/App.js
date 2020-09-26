23c18a7d3dcabf15377b97b4f0e800df51ac5c5f
dmajkowski <dmajkowski@onet.pl>
2020-09-26 10:45:05 +0200

    board styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header.js";
+import Board from "./components/Board";
 
 function App() {
   const [score, setScore] = useState(2);
   return (
     <div className="App">
       <Header score={score} />
-      {/* <Board/> */}
+      <Board />
     </div>
   );
 }
---
+++ src/components/Board/Board.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Board extends Component {
+  render() {
+    return <div className="board">Hello!</div>;
+  }
+}
+
+export default Board;
